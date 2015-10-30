9d4899f06eb06aca8c1c9895966d47831c4bcb77
Chumilion <2016esison@tjhsst.edu>
2015-10-30 11:12:21 -0400

    Initial commit... Simple intent done

--- app/src/main/java/chumilion/intents/MainActivity.java
+++ app/src/main/java/chumilion/intents/MainActivity.java
@@ -1,13 +1,16 @@
 package chumilion.intents;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+        TextView maintext = (TextView) findViewById(R.id.maintext);
+        maintext.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                Log.i("onClick", "Switching to SecondActivity...");
+                startActivity(intent);
             }
         });
     }
